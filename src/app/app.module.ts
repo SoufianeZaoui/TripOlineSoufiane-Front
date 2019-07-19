8a8b9e565aaf6a4de4d928ee3fc4b4604fe8dbe0
Soufiane Zaoui <za.soufiane@gmail.com>
2019-07-19 10:06:40 +0200

    Commit du tavail J1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TripsListComponent } from './components/trips-list/trips-list.component';
+import { HttpClient} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    TripsListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClient
+
   ],
   providers: [],
   bootstrap: [AppComponent]
